cc14d382402e5d4e357296f486e550bd627f4b0e
Vanya Klimenko <k60+git@fmap.me>
2021-10-10 16:41:07 +0300

    init packages straight in init.el
    rather than doing so in k60/packages.el which could be loaded in
    random order, i.e. after other .el files containing package
    declarations in them

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,5 +1,5 @@
 ;;; emacsd --- k60's emacs configuration
-;;; Code
+;;; Code:
 ;; -*- lexical-binding: t; -*-
 
 (setq user-full-name "Vanya Klimenko"
@@ -10,5 +10,25 @@
 (setq gc-cons-threshold (mb 50))
 (setq large-file-warning-threshold (mb 100))
 
-(setq custom-file "custom.el")
+(setq custom-file "~/.emacs.d/custom.el")
+
+(setq package-enable-at-startup nil)
+(require 'package)
+(setq package-archives (list '("melpa" . "http://melpa.org/packages/")
+                 '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+;; (unless (package-installed-p 'use-package)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
+
+(use-package exec-path-from-shell
+  :config (exec-path-from-shell-initialize))
+
+(eval-when-compile
+  (require 'use-package))
+
+(setq use-package-always-ensure t)
+
+(setq load-prefer-newer t)
+(byte-recompile-directory "~/.emacs.d/k60")
 (mapc 'load (file-expand-wildcards "~/.emacs.d/k60/*.el"))
