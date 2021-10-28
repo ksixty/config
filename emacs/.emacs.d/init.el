5502a2377f04bf2b4659e5c34f358e05594243e7
Vanya Klimenko <k60+git@fmap.me>
2021-10-28 03:45:23 +0300

    emacs: set warning level to emergency

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,23 +5,27 @@
 (setq user-full-name "Vanya Klimenko"
       user-mail-address "k60@fmap.me")
 
+(setq warning-minimum-level :emergency)
+
 (defmacro mb (n) (* n 1000000))
 
 (setq gc-cons-threshold (mb 50))
 (setq large-file-warning-threshold (mb 100))
 
 (setq custom-file "~/.emacs.d/custom.el")
+(add-to-list 'load-path "~/.emacs.d/site-lisp")
 
 (setq package-enable-at-startup nil)
 (require 'package)
 (setq package-archives (list '("melpa" . "http://melpa.org/packages/")
-                 '("gnu" . "http://elpa.gnu.org/packages/")))
+                        '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 ;; (unless (package-installed-p 'use-package)
 ;;   (package-refresh-contents)
 ;;   (package-install 'use-package))
 
 (use-package exec-path-from-shell
+  :ensure t
   :config (exec-path-from-shell-initialize))
 
 (eval-when-compile
